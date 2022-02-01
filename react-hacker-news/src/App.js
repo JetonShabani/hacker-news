4fbe8bf898894e999f53b9c93113f7ee8920c065
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-02-01 16:37:56 +0100

    First try at implementing fetch

--- react-hacker-news/src/App.js
+++ react-hacker-news/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 import Searchbar from "./components/Searchbar.js";
 import NewsList from "./components/News-List.js";
+import {useEffect} from "react";
 function App() {
+  function getData(){
+    fetch("http://hn.algolia.com/api/v1/search?query=React").then(response => response.json()).then(response=> console.log(response)).catch(err=>console.log(err));
+
+  }
+  useEffect(()=>{
+    getData();
+  },[]);
   return (
     <div className="App">
       <Searchbar/>
