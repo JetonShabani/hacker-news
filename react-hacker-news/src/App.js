fd7e4dc23d19362b7c2db4be6eb42103c12380a8
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-02-03 13:41:58 +0100

    Merge branch 'main' of https://github.com/JNpudschun/hacker-news

--- react-hacker-news/src/App.js
+++ react-hacker-news/src/App.js
@@ -1,27 +1,36 @@
 import './App.css';
 import Searchbar from "./components/Searchbar"
-
+import axios from 'axios';
 import NewsList from "./components/News-List";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 function App() {
   let dataList=[];
+  const [hits, setHits] = useState([]);
   
   async function getData(input="React"){
     let url = "http://hn.algolia.com/api/v1/search?query="+input;
-    const response = await fetch(url);
-    let data = await response.json();
-    console.log(data.hits);
-   
-   return data;
+    // const response = await fetch(url);
+    // let data = await response.json();
+    // fetch(url).then(response => response.json).then(response => setHits(response.body))
+   //return dataList;
+   axios.get(url)
+      .then(res => {
+        const hits = res.data;
+        setHits(hits.hits);
+      })
+      // console.log(hits);
   }
   useEffect(()=>{
     getData();
+    
   },[]);
+  
+  console.log(hits);
   return (
     <div className="App">
       <h1>Hello</h1>
       {/* <Searchbar/> */}
-      {/* <NewsList newsArr={dataList.hits}/> */}
+      <NewsList newsArr={hits}/>
     </div>
   );
 }
--- react-hacker-news/src/components/News.js
+++ react-hacker-news/src/components/News.js
@@ -3,7 +3,7 @@
 function News({news}){
     return (
         <li className='list-item'>
-            <span><a>title</a><a>url</a></span>
+            <span><a>title</a><a>url</a></span><br/>
             <span><a>author</a><a>creation date</a></span>
         </li>
         
