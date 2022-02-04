28069409e1926e544b51c0f98f3e2058ac118657
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-02-04 10:19:28 +0100

    passing fetchFunction into searchbar

--- react-hacker-news/src/App.js
+++ react-hacker-news/src/App.js
@@ -5,27 +5,27 @@ import NewsList from "./components/News-List";
 import {useEffect, useState} from "react";
 function App() {
   const [hits, setHits] = useState([]);
+
   
-  async function getData(input="React"){
+  function getData(input = "React"){
     let url = "http://hn.algolia.com/api/v1/search?query="+input;
+    console.log(url)
     axios.get(url)
       .then(res => {
-        const hits = res.data;
-        setHits(hits.hits);
+        // const arr = ;
+        setHits(res.data.hits);
+        console.log(hits);
+        // console.log(arr.hits);
       })
-      // console.log(hits);
   }
   useEffect(()=>{
     getData();
-    
+    console.log(hits);
   },[]);
-
-  
-  console.log(hits);
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Searchbar/>
+      <Searchbar getData={getData}/>
       <NewsList newsArr={hits}/>
 
     </div>
--- react-hacker-news/src/components/Searchbar.js
+++ react-hacker-news/src/components/Searchbar.js
@@ -1,7 +1,28 @@
+import { useState} from 'react'
 
-function Searchbar(){
+function Searchbar(props){
+    const [input, setInput] = useState(" ");
+
+    function onInputChange(e) {
+         setInput(e.target.value)
+         console.log(input)
+      }
+    //   function onClick(){
+    //     let url = "http://hn.algolia.com/api/v1/search?query="+document.getElementById("header-search").value;
+    //     console.log(url);
+    //     console.log(url)
+    //     axios.get(url)
+    //       .then(res => {
+    //         //const hits = res.data;
+    //         props.handelChange(res.data.hits);
+            
+    //       })
+    //       console.log(" Get got called");
+          
+    //   }
+      
     return(
-        <form action="/" method="get">
+        <form>
         <label htmlFor="header-search">
             <span>Search</span>
         </label>
@@ -9,8 +30,9 @@ function Searchbar(){
             type="text"
             id="header-search"
             placeholder="Search blog posts" 
+            onChange={onInputChange}
         />
-        <button type="submit">Search</button>
+        <button onClick={()=>props.getData(input)} type="submit">Search</button>
     </form>
     )   
 }
